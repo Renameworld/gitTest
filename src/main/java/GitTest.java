ea9b6ad6dc6f7de3e6426fb6bb7cde7f4c5ae20b
icebang <icebang@lbw.com>
2022-03-29 09:35:09 +0800

    MasterCommitFirst

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello gitTwo!");
         System.out.println("hello gitThree!");
+        System.out.println("MergeTest!");
     }
 }
